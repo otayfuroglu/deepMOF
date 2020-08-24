53942bc69c09b4301d30aa18a897ab9a1ee528cb
otayfuroglu <tayfuroglu@gmail.com>
2020-08-24 16:44:43 +0300

    calculation of schnet last version models is available

--- HDNNP/schnetpack/calc_SP_with_models.py
+++ HDNNP/schnetpack/calc_SP_with_models.py
@@ -11,37 +11,72 @@ from ase.collections import g2
 from schnetpack.datasets import *
 import schnetpack as spk
 
+import pandas as pd
+from scipy import stats
+from matplotlib import pyplot as plt
+
 import os, warnings
 index_warning = 'Converting sparse IndexedSlices'
 warnings.filterwarnings('ignore', index_warning)
 
 # path definitions
-model_schnet = load_model("./from_truba/mof5_model_hdnnp_forces_1/best_model")
+model_schnet = load_model("./mof5_model_hdnnp_forces_v4/best_model")
 #model_schnet = load_model("./ethanol_model/best_model")
-properties = ["energy"]#, "forces"]  # properties used for training
+properties = ["cohesive_E_perAtom"]#, "forces"]  # properties used for training
 
 
-calc_schnet = SpkCalculator(model_schnet, device="cuda", energy="energy", forces="forces", collect_triples=True)
+calc_schnet = SpkCalculator(model_schnet, device="cuda",
+                            energy=properties[0],
+                           # forces="forces",
+                            collect_triples=True)
 
 
 #path_to_db = "data/ethanol.db"
 #dataset = spk.datasets.MD17("data/ethanol.db", load_only=properties, molecule="ethanol", collect_triples=True)
-path_to_db = "../prepare_data/non_equ_geom_energy_forces.db"
+path_to_db = "../prepare_data/non_equ_geom_energy_coh_energy_forces_withORCA_v4.db"
+data = AtomsData(path_to_db)
 
-data = connect(path_to_db)
-datalist = data.select()
+column_names = ["qm_SP_energies", "schnet_SP_energies"]
+csv_file_name = "qm_sch_SP_energies"
 
-for i, atoms in enumerate(datalist):
-    #atoms_for_schnet = atoms.toatoms()
-    atoms_for_schnet = data.get_atoms(i+1)
-    atoms_for_schnet.set_calculator(calc_schnet)
+SP_energies = []
+schnet_SP_energies = []
+for i in range(len(data)):
+    #row_for_schnet = atoms.toatoms()
+    mol = data.get_atoms(i)
+    mol.set_calculator(calc_schnet)
+
+    coh_E = float(data[i][properties[0]][0])
+    coh_schnet_E = float(mol.get_potential_energy()[0])
+    
+    SP_energies.append(coh_E)
+    schnet_SP_energies.append(coh_schnet_E)
+
     if i == 0:
         print("{0:10}{1}\n".format("energy", "predict_schnet"))
-    print("{:.5f}: {:.5f}".format(
-        atoms.energy,
-        #dataset[i]["energy"][0],
-        atoms_for_schnet.get_potential_energy()[0],
-    ))
-    if i == 10:
+    print("{:.5f}: {:.5f}".format(coh_E, coh_schnet_E))
+    if i == 10000:
         break
 
+df_data = pd.DataFrame()
+df_data[column_names[0]] = SP_energies
+df_data[column_names[1]] = schnet_SP_energies
+df_data.to_csv(csv_file_name)
+
+
+
+def plot_linear_reg():
+    df_data = pd.read_csv(csv_file_name)
+    edges = [-5.5, -4.0]
+    linreg = stats.linregress(df_data[column_names[0]], df_data[column_names[0]])
+    ax = df_data.plot.scatter(x=column_names[0],
+                        # xlim=(edges[0], edges[1]),
+                        # ylim=(edges[0], edges[1]),
+                         y=column_names[1],
+                         #colormap='viridis',
+                        )
+    ax.plot(edges, edges, ls="-", color="b")
+    plt.text(-2, -3.5, "R^2=%.2f"%linreg.rvalue)
+    plt.show()
+    #plt.savefig("%s_QM_E.png"%keyword)
+plot_linear_reg()
