51962262426e08b7e989ee5b8cca2a75af4b4f58
otayfuroglu <tayfuroglu@gmail.com>
2020-08-25 18:15:45 +0300

    minor changes

--- HDNNP/schnetpack/plot_loss_curves.py
+++ HDNNP/schnetpack/plot_loss_curves.py
@@ -1,4 +1,4 @@
- #
+#
 from ase.db import connect
 import numpy as np
 import pandas as pd
@@ -10,23 +10,27 @@ sns.set()
 sns.set_style("darkgrid")
 sns.set_context("paper")
 
-df = pd.read_csv("./mof5_model_hdnnp_forces_v4/log.csv")[["Time", "Train loss", "Validation loss"]]
+df = pd.read_csv("./mof5_model_hdnnp_forces_v4_200/log.csv")#[["Time", "Train loss", "Validation loss"]]
+n_epoch = df.shape[0]
+print(n_epoch)
+
 #df2 = data=pd.melt(df, "Time")
 #print(df2.head())
 
 #Validation loss
 def plot_losses():
-
+    x = range(n_epoch)
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    #ax.set_ylim(0, 1)
-    #line1, =  ax.plot(df["Train loss"], label="Train loss")
-    #line2, =  ax.plot(df["Validation loss"], label= "Validation loss")
+    ax.set_ylim(0, 1.1)
+    ax.plot(x, df["Train loss"], label="Train loss")
+    ax.plot(x, df["Validation loss"], label= "Validation loss")
+    ax.plot(x, df["MAE_cohesive_E_perAtom"], label= "Error")
 
     # same result with seaborn
-    palette = sns.color_palette("bright", 2)
-    sns.lineplot(ax=ax, x="Time", y="value",  hue='variable', markers=True,
-                 palette=palette, data=pd.melt(df, "Time"))
-    ax.xaxis.set_major_formatter(ticker.EngFormatter())
+    #palette = sns.color_palette("bright", 2)
+    #sns.lineplot(ax=ax, x="Time", y="value",  hue='variable', markers=True,
+    #             palette=palette, data=pd.melt(df, "Time"))
+    #ax.xaxis.set_major_formatter(ticker.EngFormatter())
     plt.show()
 plot_losses()
