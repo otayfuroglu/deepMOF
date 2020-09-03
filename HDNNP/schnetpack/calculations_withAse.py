086d1fb26dbb1e7ed84872d7cbbdf704cb4c0bf2
otayfuroglu <tayfuroglu@gmail.com>
2020-09-03 17:22:43 +0300

    we make it into modlue

--- HDNNP/schnetpack/calculations_withAse.py
+++ HDNNP/schnetpack/calculations_withAse.py
@@ -14,19 +14,10 @@ References
 
 """
 
-from schnetpack.data.atoms import AtomsConverter
-from schnetpack.utils.spk_utils import DeprecationHelper
-from schnetpack import Properties
 from schnetpack.interfaces import SpkCalculator
-from schnetpack.utils import load_model
-from schnetpack.datasets import AtomsData
 
-from ase import units
-from ase.calculators.calculator import Calculator, all_changes
-from ase.io.trajectory import Trajectory
 from ase.io.xyz import read_xyz,  write_xyz
 from ase.io import read
-
 from ase.optimize import BFGS, LBFGS, GPMin, QuasiNewton
 
 import os
@@ -105,30 +96,4 @@ class AseCalculations(object):
     def print_calc(self):
         print(self.molecule.get_potential_energy())
 
-model_schnet = load_model("./models/from_truba/mof5_model_hdnnp_forces/best_model")
-#properties = ["cohesive_E_perAtom", "energy"]#, "forces"]  # properties used for training
-properties = ["energy", "forces"]  # properties used for training
 
-molecule_path="../prepare_data/mof_fragments/mof5_f1.xyz"
-working_dir="./test"
-
-#path_to_db = "../prepare_data/non_equ_geom_energy_coh_energy_forces_withORCA_v4.db"
-#data = AtomsData(path_to_db)
-#db_atoms = data.get_atoms(0)
-
-calculation = AseCalculations(working_dir,
-                              molecule_path=molecule_path
-                              #db_atoms=db_atoms,
-                             )
-calculation.adjust_calculator(properties=properties, ml_moldel=model_schnet)
-calculation.optimize()
-calculation.print_calc()
-
-def test():
-    calc_schnet = SpkCalculator(model_schnet, device="cuda",
-                                energy=properties[1],
-                               # forces="forces",
-                                collect_triples=True)
-    db_atoms.set_calculator(calc_schnet)
-    print(db_atoms.get_potential_energy())
-#test()
