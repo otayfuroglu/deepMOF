f012544306c43e2ff4a1ecf520b87686ee4c53c2
otayfuroglu <tayfuroglu@gmail.com>
2021-12-21 10:21:30 +0300

    qucik test for deepmof models

---
+++ deepmof_quicktest.py
@@ -0,0 +1,46 @@
+#
+__author__ = 'otayfuroglu'
+from schnetpack.interfaces import SpkCalculator
+from schnetpack.utils import load_model
+from schnetpack import Properties
+from schnetpack.datasets import AtomsData
+from schnetpack.environment import AseEnvironmentProvider
+
+
+import  ase
+from ase import units
+from ase.io import read, write
+from ase.optimize import BFGS, LBFGS
+
+
+# Read MOF from cif file
+irmof1 = read("./IRMOFSeries/IRMOF1.cif")
+
+# Load model
+model = load_model("./NNPotentials/deepMOF_v1/best_model")
+
+# Get calculator
+calculator = SpkCalculator(
+    model_schnet, device=device,
+    energy=Properties.energy,
+    forces=Properties.forces,
+    environment_provider=AseEnvironmentProvider(cutoff=6.0)
+)
+
+# Set calculator
+irmof1.set_calculator(calculator)
+
+# Calculate energy
+ei = irmof1.get_potential_energy()
+print("Initial Energy: ",ei)
+
+# Optimize MOF
+print("Optimizing...")
+dyn = LBFGS(irmof1)
+dyn.run(fmax=0.01)
+print("Optimization finished")
+
+# Calculate energy
+ef = irmof1.get_potential_energy()
+print("Final Energy: ",ef)
+
